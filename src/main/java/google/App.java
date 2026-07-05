ed0b6dd8995882bfc5a0c3260457a3f204895191
ranu-001 <ranjitavaddebail001@gmail.com>
2026-07-05 21:01:52 +0530

    done

--- src/main/java/google/App.java
+++ src/main/java/google/App.java
@@ -2,6 +2,6 @@ package google;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Devops");
     }
 }
